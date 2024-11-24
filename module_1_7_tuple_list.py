27b94391df963cc1be718b4c7146059dd3b8dba2
Aleksey <AlekseyTsymbalov@yahoo.com>
2024-11-24 17:42:17 +0300

    Add new code

---
+++ module_1_7_tuple_list.py
@@ -0,0 +1,7 @@
+list_0 = []
+list_0.extend("Bagration") # Если передать один аргумент, то метод выводит каждый элемент отдельно
+list_1 = ["Filya", "Mysya"]
+list_1.extend(["Gavrusha", True]) # Если поместить аргументы в [ ], то он их объединит
+print(list_0)
+print(list_1)
+print("Mysya" in list_1)
