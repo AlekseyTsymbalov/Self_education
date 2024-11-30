4a283aa0178cc488eed990b294d38464df6f57d3
Aleksey <AlekseyTsymbalov@yahoo.com>
2024-11-30 11:36:20 +0300

    Add new code

--- module_1_99_Final.py
+++ module_1_99_Final.py
@@ -1,3 +1,12 @@
 from statistics import mean
 # Функция mean() принимает список, кортеж или набор данных, содержащий числовые значения, в качестве параметра и
 # возвращает среднее значение элементов данных
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = sorted({'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'})
+average = []
+
+for digits in grades:
+    average.append(mean(digits))
+
+
+print(dict(zip(students, average)))
